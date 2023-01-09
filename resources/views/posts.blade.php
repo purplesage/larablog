dbf3b8d8b13add148acc01dd3b717b4215d2728a
purpleSage <hectormza21@gmail.com>
2023-01-09 18:18:22 -0400

    removed comments

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,4 @@
   <x-layout>
-    {{-- <x-header :currentCategory="$currentCategory" :categories={{isset($categories) ?? "$categories" : null}} /> --}}
     @include('/components/header')
 
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
@@ -12,16 +11,3 @@
     @endif
   </main>
   </x-layout>
-  
-  
-  
-  
-  {{-- @foreach ($posts as $post)
-    <article>
-      <a href="/post/{{$post->slug}}">
-        <h2>{{$post->title}}</h2>
-      </a>
-      Wrote by <a href="/authors/{{$post->author->userName}}">{{$post->author->name}}</a> in <a href="#">{{$post->category->name}}</a>
-      <p>{{$post->excerpt}}</p>
-    </article>
-  @endforeach --}}
